1e3abdd187fec791f277351691e8432921253a8e
abderrahim-ait <91316052+abderrahim-ait@users.noreply.github.com>
2021-12-06 17:43:53 +0100

    Update Carte_interactif.py

--- Package/Script/Carte_interactif.py
+++ Package/Script/Carte_interactif.py
@@ -8,16 +8,30 @@ import openrouteservice
 import json
 from ipywidgets import interact
 #%%
-# importer la base de données coordonnées.csv
-geo = pd.read_csv('./coordonnées.csv')
+# importer la base de données coordonnees.csv
+url = 'https://raw.githubusercontent.com/SENEAssane/ProjectGroup7/main/Package/Data/Base.csv'
+path = os.path.join(os.getcwd(),'Base.csv')
+download(url, path, replace=True)
+geo = pd.read_csv('./Base.csv')
 geo = geo.rename(columns={' NOMGARE ':'NOM GARE'})
+# importer la base de données Data_distance.csv
+ur1 = 'https://raw.githubusercontent.com/SENEAssane/ProjectGroup7/main/Package/Data/Datadistance.csv'
+path = os.path.join(os.getcwd(),'Datadistance.csv')
+download(ur1, path, replace=True)
+Dist = pd.read_csv('./Datadistance.csv')
+# importer la base de données Data_distance.csv
+ur1 = 'https://raw.githubusercontent.com/SENEAssane/ProjectGroup7/main/Package/Data/dataprixnettoye.csv'
+path = os.path.join(os.getcwd(),'dataprixnettoye.csv')
+download(ur1, path, replace=True)
+prix = pd.read_csv('./dataprixnettoye.csv')
+
 
 # Crée une liste qui contient le nom de toutes les villes
 villes = sorted(geo.NOMGARE.unique())
 
 def itineraire(DEPART, ARRIVEE):
-    i=geo.loc[geo['NOMGARE'] == DEPART].index[0]
-    j=geo.loc[geo['NOMGARE'] == ARRIVEE].index[0]
+    i = geo.loc[geo['NOMGARE'] == DEPART].index[0]
+    j = geo.loc[geo['NOMGARE'] == ARRIVEE].index[0]
     
     # Résolution du problème de la distance différente entre
     # l'aller et le retour avec une boucle
@@ -47,18 +61,20 @@ def itineraire(DEPART, ARRIVEE):
 
                 geometry = client.directions(coord)['routes'][0]['geometry']
                 decoded = convert.decode_polyline(geometry)
-
-                folium.GeoJson(decoded).add_child(folium.Popup(max_width = 300)).add_to(m)
+                distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(Dist.iloc[i][j+1])+" Km </strong>" +"</h4></b>"
+                prix_txt = "<h4> <b>Prix :&nbsp" + "<strong>"+str(prix.iloc[i][j+1])+" €. </strong>" +"</h4></b>"
+                prix_moy_txt = "<h4> <b>Prix Moyenne par KM :&nbsp" + "<strong>"+str(round(prix.iloc[i][j+1]/Dist.iloc[i][j+1],3))+" €. </strong>" +"</h4></b>"
+                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt+prix_txt+prix_moy_txt,max_width = 300)).add_to(m)
 
                 folium.Marker(
                             coord[0][::-1],
-                            popup=DEPART,
+                            popup="<strong>location One</strong>",
                             tooltip=DEPART
                              ).add_to(m)
 
                 folium.Marker(
                             coord[1][::-1],
-                            popup=ARRIVEE,
+                            popup="<strong>location One</strong>",
                             tooltip=ARRIVEE
                              ).add_to(m)
         return m
@@ -88,23 +104,29 @@ def itineraire(DEPART, ARRIVEE):
 
                 geometry = client.directions(coord)['routes'][0]['geometry']
                 decoded = convert.decode_polyline(geometry)
+                distance_txt = "<h4> <b>Distance :&nbsp" + "<strong>"+str(Dist.iloc[i][j+1])+" Km </strong>" +"</h4></b>"
+                prix_txt = "<h4> <b>Prix :&nbsp" + "<strong>"+str(prix.iloc[i][j+1])+" €. </strong>" +"</h4></b>"
+                prix_moy_txt = "<h4> <b>Prix Moyenne par KM :&nbsp" + "<strong>"+str(round(prix.iloc[i][j+1]/Dist.iloc[i][j+1],3))+" €. </strong>" +"</h4></b>"
 
-                folium.GeoJson(decoded).add_child(folium.Popup(max_width = 300)).add_to(m)
+                folium.GeoJson(decoded).add_child(folium.Popup(distance_txt,max_width = 300)).add_to(m)
 
                 folium.Marker(
                             coord[0][::-1],
-                            popup=DEPART,
-                            tooltip=DEPART
+                            popup="<strong>location One</strong>",
+                            tooltip=ARRIVEE 
                              ).add_to(m)
 
                 folium.Marker(
                             coord[1][::-1],
-                            popup=ARRIVEE,
-                            tooltip=ARRIVEE
+                            popup="<strong>location One</strong>",
+                            tooltip=DEPART
                              ).add_to(m)
+                     
         return m
     
     else :
         print("Veuilliez entrer deux sorties différentes")  
 
 interact(itineraire, DEPART = villes, ARRIVEE = villes)
+
+# %%
